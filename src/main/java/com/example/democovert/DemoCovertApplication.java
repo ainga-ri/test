48379c06b6a666f0319e7c66c17be98719091804
ainga-ri <alfred.inga.rios@gmail.com>
2023-06-20 15:57:27 +0200

    test test

---
+++ src/main/java/com/example/democovert/DemoCovertApplication.java
@@ -0,0 +1,13 @@
+package com.example.democovert;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DemoCovertApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(DemoCovertApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/example/democovert/DemoCovertApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.democovert;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DemoCovertApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
