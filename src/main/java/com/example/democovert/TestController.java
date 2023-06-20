95eb87ca8de38944340f6b41cb08d6e5b2f85480
ainga-ri <alfred.inga.rios@gmail.com>
2023-06-20 17:45:11 +0200

    check error line 1ae

--- src/main/java/com/example/democovert/TestController.java
+++ src/main/java/com/example/democovert/TestController.java
@@ -1,12 +1,14 @@
 package com.example.democovert;
 
 import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
 @RequestMapping("/example")
+@CrossOrigin(origins = "*")
 public class TestController {
 
     @GetMapping
